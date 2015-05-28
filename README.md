a6e5239b344c64ccf55040d691227eb32f102ee4
Dye Jarhoo <20123225@student.shiep.edu.cn>
2015-05-28 20:21:50 +0800

    init just work release

---
+++ README.md
@@ -0,0 +1,9 @@
+# TXTproxy
+A gateway website which redirecting to one's domain name TXT record.
+
+
+## Usage:
+1. execute txtproxy.py
+2. goto http://<your_website_addr_with_port>/tp/<domain_name_with_TXT_record>
+3. wait and boom you have been redirected or given the 400 error page
+
---
+++ txtproxy.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Domainname redirect according it's TXT record
+# version 0.0.1
+# Author: dye Jarhoo
+
+from flask import Flask, render_template, url_for, abort
+import dnsq
+
+# Fireup our little app ;)
+app = Flask(__name__)
+
+
+def checkdn(str):
+    """return True if str is a domain"""
+
+    if str[-1] == '.':
+        str = str[:-1]
+    if len(str.split('.')) > 1:
+        return True
+
+
+@app.route('/tp/<req>')
+def textproxy(req):
+    """Input url, return redirect html"""
+    if checkdn(req):
+        arrayQry = dnsq.query_dns(req, 'TXT')
+        if arrayQry:
+            return render_template('redirect.html', url=arrayQry[0])
+
+    return abort(400)
+
+
+@app.route('/favicon.ico')
+def offerFavicon():
+    """Offer Favicon file"""
+    url_for('static', filename='favicon.ico')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
